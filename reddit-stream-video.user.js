c94a2266c96b78b34f9cbad7dd93d8ca265202b2
unknown <kike@asier-sistemas.intranetorbere.com>
2014-06-05 09:40:55 +0200

    Twitch.tv link checker added.

--- reddit-stream-video.user.js
+++ reddit-stream-video.user.js
@@ -33,21 +33,25 @@ function getObjectHeigth(objW){
     return Math.round(9 / 16 * objW);
 }
 function addEmbed(embed) {
-    var main = document.getElementById('c-main');
-    var sidebar = document.getElementById('sidebar');
-    main.style.width="36%";
-    main.style.margin="0px 0px 0px 30px";
-    sidebar.style.width="60%";
-    sidebar.style.left="40%";
     var l = getLocation(embed);
-    console.log(embed);
-    embed = l.pathname;
-    embed = "<object type='application/x-shockwave-flash' width='"+getObjectWidth()+"' height='"+getObjectHeigth(getObjectWidth())+"' id='live_embed_player_flash' data='http://www.twitch.tv/widgets/live_embed_player.swf?channel=" + embed + "' bgcolor='#000000'><param name='allowFullScreen' value='true' /><param name='allowScriptAccess' value='always' /><param name='allowNetworking' value='all' /><param name='movie' value='http://www.twitch.tv/widgets/live_embed_player.swf' /><param name='flashvars' value='hostname=www.twitch.tv&channel=" + embed + "&auto_play=true&start_volume=25' /></object>";
-    removeSidebar();
-    var newDiv = document.createElement('div');
-    newDiv.id = 'twitchEmbedd';
-    newDiv.innerHTML=embed;
-    document.getElementById('sidebar').appendChild(newDiv);
+    if(l.hostname != "www.twitch.tv" || l.hostname != 'twitch.tv'){
+        alert("Please type a valid twitch.tv link.");
+    }else{
+        var main = document.getElementById('c-main');
+        var sidebar = document.getElementById('sidebar');
+        main.style.width="36%";
+        main.style.margin="0px 0px 0px 30px";
+        sidebar.style.width="60%";
+        sidebar.style.left="40%";
+        console.log(embed);
+        embed = l.pathname;
+        embed = "<object type='application/x-shockwave-flash' width='"+getObjectWidth()+"' height='"+getObjectHeigth(getObjectWidth())+"' id='live_embed_player_flash' data='http://www.twitch.tv/widgets/live_embed_player.swf?channel=" + embed + "' bgcolor='#000000'><param name='allowFullScreen' value='true' /><param name='allowScriptAccess' value='always' /><param name='allowNetworking' value='all' /><param name='movie' value='http://www.twitch.tv/widgets/live_embed_player.swf' /><param name='flashvars' value='hostname=www.twitch.tv&channel=" + embed + "&auto_play=true&start_volume=25' /></object>";
+        removeSidebar();
+        var newDiv = document.createElement('div');
+        newDiv.id = 'twitchEmbedd';
+        newDiv.innerHTML=embed;
+        document.getElementById('sidebar').appendChild(newDiv);
+    }
 }
 function addButton() {
     var bar = document.getElementById('user-bar');
